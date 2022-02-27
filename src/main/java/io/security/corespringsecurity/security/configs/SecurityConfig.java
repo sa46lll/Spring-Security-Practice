63c71869d8be57b5bca6014602ab0fd09b5a2bd9
sa46lll <9812smh@naver.com>
2022-02-27 16:06:46 +0900

    feat: 사용자 DB 등록, PasswordEncoder

--- src/main/java/io/security/corespringsecurity/security/configs/SecurityConfig.java
+++ src/main/java/io/security/corespringsecurity/security/configs/SecurityConfig.java
@@ -1,10 +1,12 @@
 package io.security.corespringsecurity.security.configs;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -29,10 +31,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Override
+    public void configure(WebSecurity web) throws Exception {
+        web
+                .ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations())
+        .and()
+                .ignoring().antMatchers("/h2-console/**");
+    }
+
+    @Override
     protected void configure(final HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/").permitAll()
+                .antMatchers("/", "/users").permitAll()
                 .antMatchers("/mypage").hasRole("USER")
                 .antMatchers("/messages").hasRole("MANAGER")
                 .antMatchers("/config").hasRole("ADMIN")
